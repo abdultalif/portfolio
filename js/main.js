99799198b1d3a477172c55582b642357a4c25988
abdultalif <abdultalif85@gmail.com>
2023-05-26 17:31:08 +0700

    mengisi menu sertifikasi

--- js/main.js
+++ js/main.js
@@ -89,3 +89,84 @@ function getPortfolio(name) {
         portfolioModal.show();
     })
 }
+
+
+const certificationMenu = document.getElementById('certificationMenu');
+certificationMenu.innerHTML = '';
+axios.get(window.location.origin + '/certifications.json').then((response) => {
+    for (var key in response.data) {
+        certificationMenu.innerHTML += `
+            <div class="col-md-6 col-lg-4 mb-5">
+                <div class="certification-item mx-auto" onclick="return getCertification('${key}')">
+                    <div class="certification-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="certification-item-caption-content text-center text-white">
+                            <i class="fas fa-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="assets/img/certifications/${key}/${response.data[key].images[0]}" alt="${response.data[key].title}" />
+                </div>
+            </div>
+        `;
+    }
+});
+
+let certificationModal = new bootstrap.Modal(document.getElementById("certificationModal"), {});
+
+function getCertification(name) {
+    axios.get(window.location.origin + '/certifications.json').then((response) => {
+        let certification = response.data[name];
+
+        // Update data in the modal
+        let modal = document.getElementById('certificationModal');
+
+        modal.querySelector('#certificationDesc').innerHTML = certification.desc;
+        modal.querySelector('#certificationTitle').innerHTML = certification.title;
+        modal.querySelector('#certificationLabel').innerHTML = '<i class="fa fa-tag me-2"></i>' + certification.label;
+        modal.querySelector('#certificationLink').href = certification.link;
+
+
+        // Loop for Tech
+        const certificationTech = modal.querySelector('#certificationTech');
+        certificationTech.innerHTML = "";
+        certification.tech.forEach(tech => {
+            certificationTech.innerHTML += `
+                <li class="list-inline-item">
+                    &bull;
+                    ${tech}
+                </li>`;
+        });
+
+
+
+        // Loop for Certification Images
+        const certificationImages = modal.querySelector('#certificationImages');
+        const certificationIndicators = modal.querySelector('#certificationIndicators');
+        certificationImages.innerHTML = "";
+        certificationIndicators.innerHTML = "";
+        certification.images.forEach((img, index) => {
+            certificationImages.innerHTML += `
+                <div class="carousel-item ${index == 0 && 'active'}">
+                    <img 
+                        src="assets/img/certifications/${name}/${img}" 
+                        class="d-block w-100"
+                        alt="${name}"
+                    />
+                </div>
+            `;
+
+            certificationIndicators.innerHTML += `
+                <button 
+                    type="button" 
+                    data-bs-target="#carouselCertification"
+                    data-bs-slide-to="${index}" 
+                    ${index == 0 && 'class="active"'}
+                    aria-current="true"
+                    aria-label="${name}"
+                ></button>
+            `;
+        });
+
+        // Show the modal
+        certificationModal.show();
+    })
+}
